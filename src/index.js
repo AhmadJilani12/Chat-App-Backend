4030dcb33b16132fd7265782736cbfb6c9ed9a31
My8921 <ahmadali43a5@gmail.com>
2025-05-07 14:25:48 +0500

    setted indexjs

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 
 const allowedOrigins = [
-  'chat-app-frontend-for-production.vercel.app',
+  'https://chat-app-frontend-for-production.vercel.app',
   'https://chat-app-frontend-for-production-gru5gx1jv.vercel.app',
   'https://chat-app-frontend-for-git-d0e7fa-ahmadali43a5gmailcoms-projects.vercel.app'
 ];
