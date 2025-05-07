50bd76bbcef49571bb937b4e987798077ac0e757
My8921 <ahmadali43a5@gmail.com>
2025-05-07 23:00:09 +0500

    remove secure false

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -10,11 +10,9 @@ export const getUsersForSidebar = async (req, res) => {
 
         const filterdUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
         res.status(200).json(filterdUsers);
-
     } catch (error) {
         console.log("Error in get Users for Sidebar: ", error.message);
         res.status(500).json({ error: "Internal Server Error" });
-
     }
 };
 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const  generateToken = (userId , res) => {
         maxAge:7*24*60*60*1000,
         httpOnly:true,
         sameSite:true,
-        secure:process.env.NODE_ENV !== "development" ? true : false
+        secure: true 
     });
 
     return token;
